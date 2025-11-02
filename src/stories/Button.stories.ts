3bcc013371e418486d94a44bef61b1d1409f76bb
donghyunlee <suudh99@naver.com>
2025-11-02 18:36:44 +0900

    feat: storybook 세팅

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { fn } from 'storybook/test';
-
-import { Button } from './Button';
+import { Button } from '../primitives/button/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -16,7 +15,8 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    variant: { control: 'select', options: ['solid', 'outline', 'ghost'] },
+    size: { control: 'select', options: ['small', 'medium', 'large'] },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -26,29 +26,101 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Solid_Large: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
+    variant: 'solid',
     size: 'large',
-    label: 'Button',
+    title: 'Button',
   },
 };
 
-export const Small: Story = {
+export const Solid_Medium: Story = {
   args: {
+    variant: 'solid',
+    size: 'medium',
+    title: 'Button',
+  },
+};
+
+export const Solid_Small: Story = {
+  args: {
+    variant: 'solid',
     size: 'small',
-    label: 'Button',
+    title: 'Button',
+  },
+};
+
+export const Outline_Large: Story = {
+  args: {
+    variant: 'outline',
+    size: 'large',
+    title: 'Button',
+  },
+};
+
+export const Outline_Medium: Story = {
+  args: {
+    variant: 'outline',
+    size: 'medium',
+    title: 'Button',
+  },
+};
+
+export const Outline_Small: Story = {
+  args: {
+    variant: 'outline',
+    size: 'small',
+    title: 'Button',
+  },
+};
+
+export const Ghost_Large: Story = {
+  args: {
+    variant: 'ghost',
+    size: 'large',
+    title: 'Button',
+  },
+};
+
+export const Ghost_Medium: Story = {
+  args: {
+    variant: 'ghost',
+    size: 'medium',
+    title: 'Button',
+  },
+};
+
+export const Ghost_Small: Story = {
+  args: {
+    variant: 'ghost',
+    size: 'small',
+    title: 'Button',
+  },
+};
+
+export const Disabled_Solid: Story = {
+  args: {
+    variant: 'solid',
+    size: 'large',
+    title: 'Button',
+    disabled: true,
+  },
+};
+
+export const Disabled_Outline: Story = {
+  args: {
+    variant: 'outline',
+    size: 'large',
+    title: 'Button',
+    disabled: true,
+  },
+};
+
+export const Disabled_Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    size: 'large',
+    title: 'Button',
+    disabled: true,
   },
 };
